b6c0e7e34559d52664cc552bfe77317ebf7c7a1f
chandana-musunuru <musunuruchandana@gmail.com>
2026-02-06 19:51:18 -0500

    Initial commit

---
+++ spring-aop/spring/src/test/java/com/aop/spring/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aop.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
